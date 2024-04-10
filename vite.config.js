2bd90f0194bc8624e81d3ce72807477d9f9998fe
vanthom04 <vanthom2210@gmail.com>
2024-04-10 10:24:08 +0700

    config vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '~', replacement: '/src' }]
+  },
+  build: {
+    outDir: 'build'
+  },
+  server: {
+    host: true,
+    port: 6100
+  }
 })
