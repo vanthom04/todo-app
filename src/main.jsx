058bbf6f07ae921878b375a6bd68833be97bae96
vanthom04 <vanthom2210@gmail.com>
2024-04-12 13:14:34 +0700

    Create and config supabase, config react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Toaster position="top-center" />
   </React.StrictMode>
 )
