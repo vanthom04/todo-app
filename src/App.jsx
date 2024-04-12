7ce87c7949fa90b6a3ef8b776bece2e2850fec06
vanthom04 <vanthom2210@gmail.com>
2024-04-12 15:32:06 +0700

    create todo input and todo item

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import TodoInput from './components/TodoInput'
+import TodoItem from './components/TodoItem'
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Todo App</h1>
+    <div className="wrapper">
+      <header className="header">
+        <h1>My Todo</h1>
+        <TodoInput />
+      </header>
+      <main className="main">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </main>
     </div>
   )
 }
