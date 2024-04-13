b65489b01923fc068685ddcf5030d33144a32597
NhatQuang <qag01092004@gmail.com>
2024-04-13 09:14:05 +0700

    Create task Todo

--- src/components/TodoInput/TodoInput.jsx
+++ src/components/TodoInput/TodoInput.jsx
@@ -1,18 +1,37 @@
+import { useState } from 'react'
+import toast from 'react-hot-toast'
+import { supabase } from '~/config/supabase'
 import './TodoInput.css'
 
+
 function InputTodo() {
+  const [task, setTask] = useState('')
+
   const handleSubmit = async (e) => {
     e.preventDefault()
+    if (!task) {
+      toast.error('Please ! enter task for you')
+    }
+
+    const { error } = await supabase.from('my_todo').insert({ task:task })
+    if (error) {
+      toast.error('Insert task failure')
+    } else {
+      toast.success('Insert task successfully')
+    }
   }
 
   return (
     <form className="todo-input" onSubmit={handleSubmit}>
       <input
         type="text"
-        name="todo"
+        name={task}
+        onChange={ (e) => setTask(e.target.value)}
         placeholder="Enter todo..."
+        autoComplete="off"
+        spellCheck="false"
       />
-      <button type="submit">
+      <button onClick={handleSubmit}>
         ADD
       </button>
     </form>
